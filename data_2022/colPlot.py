c2a7ba86a293382812a5ea1cec635ff204678149
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2023-02-01 00:40:47 -0500

    Get the grain size dependence of coeffs

---
+++ data_2022/colPlot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+with open('eu.out', 'r') as f:
+    jar = f.readlines()
+
+gr = []
+qoi = [[] for i in range(6)]
+for line in jar:
+    tmp = line.split()
+    gr.append(float(tmp[0]))
+    for i in range(6):
+        qoi[i].append(float(tmp[i+1]))
+
+for i in range(6):
+    plt.plot(gr, qoi[i], label=f'{i}')
+
+plt.legend()
+plt.show()
+#plt.savefig('eureka.pdf')
--- data_2022/eureka.py
+++ data_2022/eureka.py
@@ -3,9 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+def sig(x, a, b, c):
+    return a / (1 + np.exp(-b * (x - c)))
+
 # the nominal set
 jar = pd.read_csv('nominal_set.csv')
 fd = 'Fission Density (10^21 f/cm3)'
 gr = 'Grain Size (um)'
 fgs = 'Fuel Gas Swelling (%)'
 
+for G in [1.34, 4.36, 8.5, 17, 34]:
+    # select grain size
+    tra = jar.loc[jar[gr] == G]
+
+    # cutoff based on local minimum
+    dd = np.diff(tra[fgs])
+    pivlo = len(dd) // 7 * 2
+    pivhi = pivlo * 2
+    mi = pivlo + np.argmin(dd[pivlo:pivhi])
+    cut = tra[fgs].iloc[mi]
+    print(mi, mi/132*7)
+    print(cut)
+
+    low = tra.loc[tra[fgs] <= cut]
+    high = tra.loc[tra[fgs] > cut]
+    high[fgs] = high[fgs] - cut
+
+    p1, c1 = curve_fit(sig, low[fd], low[fgs],
+                       bounds=([0,0,0], [np.inf,np.inf,np.inf]))
+    p2, c2 = curve_fit(sig, high[fd], high[fgs],
+                       bounds=([0,0,0], [np.inf,np.inf,np.inf]))
+    print(p1, p2)
+
+    pred = [sig(x, *p1) + sig(x, *p2) for x in tra[fd]]
+    plt.plot(tra[fd], tra[fgs])
+    plt.plot(tra[fd], pred, ls='-.')
+
+plt.show()
